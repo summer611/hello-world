ebdb276e3234924836fb635fedd048ba5c094329
summer <xia.he611@gmail.com>
2018-03-01 22:32:56 -0500

    completed first part of lab1 in my repository hello-world

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,10 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int x = 0;
+        while (x < 10) {
+		    int sum=sum+x;
+            System.out.print(sum + " ");
+            x = x + 1;
+        }
+    }
+}
